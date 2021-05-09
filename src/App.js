3afee621f734deda609d976f15da6283d3daafe6
chayoung-choi <chayoung.dev@gmail.com>
2021-05-09 21:55:17 +0900

    #4.1 Rendering the Movies

--- src/App.js
+++ src/App.js
@@ -1,72 +1,46 @@
 import React from "react";
-import PropTypes from "prop-types";
+import axios from "axios";
+import Movie from "./Movie";
 
-const foodILike = [
-    {
-        id: 1,
-        name: "Kimchi",
-        image:
-            "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
-    },
-    {
-        id: 2,
-        name: "Samgyeopsal",
-        image:
-            "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
-        rating: 4.9
-    },
-    {
-        id: 3,
-        name: "Bibimbap",
-        image:
-            "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
-        rating: 4.8
-    },
-    {
-        id: 4,
-        name: "Doncasu",
-        image:
-            "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
-        rating: 5.5
-    },
-    {
-        id: 5,
-        name: "Kimbap",
-        image:
-            "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
-        rating: 4.7
+class App extends React.Component {
+    state = {
+        isLoading: true,
+        movies: []
+    };
+    getMovies = async () => {
+        const {
+            data: {
+                data: {movies}
+            }
+        } = await axios.get(
+            "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+        );
+        this.setState({movies, isLoading: false});
+    };
+
+    componentDidMount() {
+        this.getMovies();
     }
-];
 
-function Food({name, picture, rating}) {
-    return (
-        <div>
-            <h2>I like {name}</h2>
-            <h4>{rating}/5.0</h4>
-            <img src={picture} alt={name}/>
-        </div>
-    );
-}
-
-Food.propTypes = {
-    name: PropTypes.string.isRequired,
-    picture: PropTypes.string.isRequired,
-    rating: PropTypes.number
-};
-
-function App() {
-    return (
-        <div>
-            {foodILike.map(dish => (
-                <Food
-                    key={dish.id}
-                    name={dish.name}
-                    picture={dish.image}
-                    rating={dish.rating}
-                />
-            ))}
-        </div>
-    );
+    render() {
+        const {isLoading, movies} = this.state;
+        return (
+            <div>
+                {isLoading
+                    ? "Loading..."
+                    : movies.map(movie => (
+                        <Movie
+                            key={movie.id}
+                            id={movie.id}
+                            year={movie.year}
+                            title={movie.title}
+                            summary={movie.summary}
+                            poster={movie.medium_cover_image}
+                        />
+                    ))}
+            </div>
+        );
+    }
 }
 
 export default App;
